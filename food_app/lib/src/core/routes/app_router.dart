a1748a2bf2c6d26e4c68708c3a9cd7692ea83f32
Anish Baniya <anishbaniya07@gmail.com>
2025-03-30 14:16:51 +0545

    start: project started

---
+++ food_app/lib/src/core/routes/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/src/features/splash/splash_page.dart';
+
+class AppRouter {
+  //This method is like a navigation guide that tells the app
+  //which page to show based on the user's request (like choosing which room to go to).
+  static Route<dynamic> route(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (context) => SplashPage());
+      default:
+        return MaterialPageRoute(builder: (context) => InvalidRoute());
+    }
+  }
+}
+
+class InvalidRoute extends StatelessWidget {
+  const InvalidRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Invalid Route'));
+  }
+}
+
+
+//Explanation
+// AppRouter is like a navigation guide that decides where to send the user 
+//when they ask to go to a specific page.
+
+// When the user asks to go to the root page ('/'), the guide sends them to the SplashPage.
+
+// If they try to go to a room that doesn’t exist, the guide sends them to an error 
+//page that says "Invalid Route."
---
+++ food_app/lib/src/core/routes/app_routes.dart
@@ -0,0 +1,17 @@
+class AppRoutes {
+  static const String initial = '/';
+  static const String home = initial;
+}
+
+//Explanation
+// Imagine your app is a big house with many rooms (pages/screens).
+
+// You need a list of room names so you can tell the app which room (page) 
+//to show when users go from one page to another.
+
+// The AppRoutes class is like a map or list where you keep track of the room names.
+
+// The first room, or the room that people enter when they come to the house, 
+//is the "initial" room (which is set as '/').
+
+// You're also calling the main room "home" in your map, but it points to the same initial room.
